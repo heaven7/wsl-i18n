cc7e9e5db815ccce7b9658cbb0a7ed9e9788cf60
Christian Grlica <christian@heavenseven.net>
2016-02-21 19:30:49 +0100

    removed core dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'heaven7:wsl-i18n',
-  version: '0.0.3_1',
+  version: '0.0.3_3',
   summary: 'I18n package',
   git: 'https://github.com/heaven7/wsl-i18n.git',
   documentation: 'README.md'
@@ -12,10 +12,10 @@ Package.onUse(function(api) {
     api.versionsFrom('1.2');
 
     var packages = [
-        'heaven7:wsl-core@0.0.3_1',
-        'heaven7:wsl-translations@0.0.3',
         'tap:i18n@1.7.0',
         'tap:i18n-ui@0.7.0',
+        'raix:handlebar-helpers@0.2.5',
+        'softwarerero:accounts-t9n@1.2.2',
         'gwendall:autoform-i18n@0.1.9_1'
     ];
 
