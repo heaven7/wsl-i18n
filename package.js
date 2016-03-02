a4429153a0462f63660a2afb7405b80b437b15dc
Christian Grlica <christian@heavenseven.net>
2016-03-02 13:33:53 +0100

    fixes for independent package use

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'heaven7:wsl-i18n',
-  version: '0.0.3_3',
+  version: '0.0.3_4',
   summary: 'I18n package',
   git: 'https://github.com/heaven7/wsl-i18n.git',
   documentation: 'README.md'
@@ -12,11 +12,14 @@ Package.onUse(function(api) {
     api.versionsFrom('1.2');
 
     var packages = [
+        'heaven7:wsl-translations@0.0.3',
         'tap:i18n@1.7.0',
         'tap:i18n-ui@0.7.0',
         'raix:handlebar-helpers@0.2.5',
         'softwarerero:accounts-t9n@1.2.2',
-        'gwendall:autoform-i18n@0.1.9_1'
+        'gwendall:autoform-i18n@0.1.9_1',
+        'ecmascript',
+        'es5-shim'
     ];
 
     api.use(packages, both);
@@ -26,7 +29,5 @@ Package.onUse(function(api) {
 
     api.addFiles('lib/both/i18n.js', both);
 
-    api.export([
-        'i18n'
-    ]);
+    api.export('i18n');
 });
