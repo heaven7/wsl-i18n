d453ace8c7a86f248b38036c80716babce7423b5
Christian Grlica <christian@heavenseven.net>
2016-03-12 15:26:25 +0100

    moved language switch into package

--- package.js
+++ package.js
@@ -4,30 +4,33 @@ Package.describe({
   summary: 'I18n package',
   git: 'https://github.com/heaven7/wsl-i18n.git',
   documentation: 'README.md'
-});
+})
 
-both = ['client','server'];
-
-Package.onUse(function(api) {
-    api.versionsFrom('1.2');
-
-    var packages = [
+var both = ['client','server'],
+    packages = [
         'heaven7:wsl-translations@0.0.3',
         'tap:i18n@1.7.0',
         'tap:i18n-ui@0.7.0',
         'raix:handlebar-helpers@0.2.5',
         'softwarerero:accounts-t9n@1.2.2',
         'gwendall:autoform-i18n@0.1.9_1',
+        'gwendall:simple-schema-i18n@0.2.1',
         'ecmascript',
         'es5-shim'
-    ];
+    ]
 
-    api.use(packages, both);
-    api.imply(packages);
+Package.onUse(function(api) {
+    api.versionsFrom('1.2')
+    api.use(packages, both)
+    api.imply(packages)
 
-    api.use('session', 'client');
+    api.use('session', 'client')
 
-    api.addFiles('lib/both/i18n.js', both);
+    api.addFiles('lib/both/i18n.js', both)
+    api.addFiles([
+        'lib/client/templates.html',
+        'lib/client/templates.js'
+    ], ['client'])
 
-    api.export('i18n');
-});
+    api.export('i18n')
+})
